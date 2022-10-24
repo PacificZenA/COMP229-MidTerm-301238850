aeb8f2486d7fccd356846c3ed504b8b7cb7cf758
Alex Tang <alextoronto66@gmail.com>
2022-10-24 14:52:51 -0400

    Connected to mogodb database

--- server/config/config.js
+++ server/config/config.js
@@ -1,2 +1,3 @@
-export const MongoURI = "mongodb://localhost:27017/media";
+//export const MongoURI = "mongodb://localhost:27017/media";
 export const Secret = "someSecret";
+export const MongoURI = "mongodb+srv://WebDevAlex:7wYdpy5IY2bHySlN@cluster0.zkqspsk.mongodb.net/media?retryWrites=true&w=majority";
